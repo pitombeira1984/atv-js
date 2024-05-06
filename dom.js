444697a60f4536177ac3f1968ef3042660b48028
pitombeira1984 <168240874+pitombeira1984@users.noreply.github.com>
2024-05-06 18:51:13 -0300

    versao 0.0.1
    Esta é uma atividade feito em sala

---
+++ dom.js
@@ -0,0 +1,32 @@
+// var name = prompt("Qual o seu nome?")
+// var age = Number(prompt("Qual a sua idade?"))
+
+
+// let rodolfo = {
+//     name: name,
+//     age: age,
+//     isHuman: true
+// }
+
+// console.log(`Olá ${rodolfo.name}, você tem ${rodolfo.age} anos `)
+// let bernardo=[
+//     "girafa",
+//     "zebra",
+//     28,
+//     "alfabeto",
+//     true
+// ]
+// console.log(`A segunda linha do seu Arrey é ${bernardo[2]}`)
+let bernardo=[
+    "girafa",
+    "zebra",
+    28,
+    "alfabeto",
+    {
+        name: "luan",
+        age: 26,
+        isAdm: true
+    },
+    true
+]
+console.log(`A segunda linha do seu Arrey é ${bernardo[4].name}`)
